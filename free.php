b5d58ab5f87959bb39b478f0fd6bd6588069b78e
melahu <93563273+Samad87@users.noreply.github.com>
2026-01-14 21:22:43 +0800

    Add stream headers for adaptive inputstream

--- free.php
+++ free.php
@@ -26,6 +26,7 @@ https://dl.dropboxusercontent.com/scl/fi/6fw9ub5kgp0t2bxr51r0a/event1.m3u8?rlkey
 
 #EXTINF:-1 group-title="" group-logo="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzJmMnB1djl6Mno1MG90bndsczZoNXN0NGx5NXZnbHNjaTM2cGhidSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/mWnDeIKilkwDcrM2VT/giphy.gif" tvg-logo="https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExMzJmMnB1djl6Mno1MG90bndsczZoNXN0NGx5NXZnbHNjaTM2cGhidSZlcD12MV9naWZzX3NlYXJjaCZjdD1n/mWnDeIKilkwDcrM2VT/giphy.gif",LIVE EVENT 
 #EXTVLCOPT:http-referrer=https://xiaolin.live/
+#KODIPROP:inputstream.adaptive.stream_headers={"Origin":"https://www.camel1.live","Referer":"https://www.camel1.live/"}
 #KODIPROP:inputstream.adaptive.manifest_type=hls
 #EXTVLCOPT:http-user-agent=Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, seperti Gecko) Chrome/108.0.0.0 Safari/537.36
 https://dl.dropboxusercontent.com/scl/fi/yi4k05tckon970a3anp6g/event2.m3u8?rlkey=buffkfzw8g2zit98m9f9tmgi6&st=lc2zudfu&dl=0
